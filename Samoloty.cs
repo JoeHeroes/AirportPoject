84a5ddecb67d196c904039e191736de26de3c6c9
KornelG <kornelg2001@wp.pl>
2021-05-18 20:50:58 +0200

    Samoloty,trasa,lotnisko

---
+++ Samoloty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Samolot
+public abstract class Samolot
+{
+	private double Zasieg;
+	private string Id;
+	private int LiczbaMiejsc
+	public Samolot(double zasieg, string id, int liczbamiejsc) { this.Zasieg = zasieg;this.Id = id;this.LiczbaMiejsc = liczbamiejsc; }
+	public double getZasieg(return this.Zasieg;)
+	public string getId(return this.Id;)
+}
+public class Dlugodystansowy : Samolot
+{ }
+public class Sredniodystansowy : Samolot
+{ }
+public class Regionalny : Samolot
+{ }
+
+
+
---
+++ Trasa,Lotnisko.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+namespace Trasa
+public class Trasa()
+{
+	private double Dystans;
+	private ArrayList Lotniska;
+	private int Czas;
+public Trasa(double dystans, int czas) { this.Dystans = dystans;this.Czas = czas; Lotniska = new ArrayList(); }
+public double getDystans() { return this.Dystans; }
+public ArrayList getLotniska() { return Lotniska; }
+public void dodajLotnisko(Lotnisko L) { Lotniska.add(L); }
+public void usunLotnisko(Lotnisko L) { Lotnisko.}
+}
+public class Lotnisko()
+{
+	private string Kraj, Miasto, Id;
+    public Lotnisko(string kraj,string miasto,string id) { this.Kraj = kraj; this.miasto = miasto; this.Id = id; }
+	public getKraj() { return this.Kraj; }
+	public getMiasto() { return this.Miasto; }
+	public getId() { return this.Id; }
+}
