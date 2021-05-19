0fae96244d2ca9f5915e3d515fefefbaafd65668
KornelG <kornelg2001@wp.pl>
2021-05-19 10:06:36 +0200

    Zmiany w Trasie, Lotnisko - ukończenie klas; nie wiem czy dobrze zrobiłem porównanie w Trasie także możecie sprawdzić xD

--- Trasa,Lotnisko.cs
+++ Trasa,Lotnisko.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections;
 namespace Trasa
-public class Trasa()
+public class Trasa
 {
 	private double Dystans;
 	private ArrayList Lotniska;
 	private int Czas;
-public Trasa(double dystans, int czas) { this.Dystans = dystans;this.Czas = czas; Lotniska = new ArrayList(); }
-public double getDystans() { return this.Dystans; }
-public ArrayList getLotniska() { return Lotniska; }
-public void dodajLotnisko(Lotnisko L) { Lotniska.add(L); }
-public void usunLotnisko(Lotnisko L) { Lotnisko.//}
+	public Trasa(double dystans, int czas) { this.Dystans = dystans;this.Czas = czas; Lotniska = new ArrayList(); }
+	public double getDystans() { return this.Dystans; }
+	public ArrayList getLotniska() { return Lotniska; }
+	public void dodajLotnisko(Lotnisko L) { Lotniska.add(L); }
+	public void usunLotnisko(Lotnisko L) {
+		foreach(Lotnisko Lo in Lotniska)
+		{
+			if (porownanieLotnisk(L, Lo))
+				Lotniska.Remove(Lo);
+		}
+	}
+	private bool porownanieLotnisk(Lotnisko L,Lotnisko Lo)
+	{return (L.getId() == Lo.getId() && L.getKraj() == Lo.getKraj() && L.getMiasto() == Lo.getMiasto());}
 }
-public class Lotnisko()
+public class Lotnisko
 {
 	private string Kraj, Miasto, Id;
     public Lotnisko(string kraj,string miasto,string id) { this.Kraj = kraj; this.miasto = miasto; this.Id = id; }
-	public getKraj() { return this.Kraj; }
-	public getMiasto() { return this.Miasto; }
-	public getId() { return this.Id; }
+	public string getKraj() { return this.Kraj; }
+	public string getMiasto() { return this.Miasto; }
+	public string getId() { return this.Id; }
 }
