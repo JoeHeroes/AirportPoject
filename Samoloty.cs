61428b034deb016f9c80129cb087b11f19009c36
KornelG <kornelg2001@wp.pl>
2021-05-26 16:13:30 +0200

    Poprawa błędów pisowni

--- Samoloty.cs
+++ Samoloty.cs
@@ -11,18 +11,24 @@ namespace LiniaLotnicza
 				throw new ZasiegException("Zasieg nie moze byc mniejszy od zera.");
 			if (liczbamiejsc <= 0)
 				throw new LiczbaMiejscException("Liczba miejsc nie moze byc mniejszy od zera.");
-			this.Zasieg = zasieg; 
-			this.Id = id; 
+			this.Zasieg = zasieg;
+			this.Id = id;
 			this.LiczbaMiejsc = liczbamiejsc; }
-		public double getZasieg(return this.Zasieg;)
-	public string getId(return this.Id;)
+		public double getZasieg() { return this.Zasieg; }
+		public string getId(){return this.Id; }
 }
 	public class Dlugodystansowy : Samolot
-	{ }
+	{
+		public Dlugodystansowy(double zasieg, string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc) { }
+	}
 	public class Sredniodystansowy : Samolot
-	{ }
+	{
+		public Sredniodystansowy(double zasieg, string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc) { }
+	}
 	public class Regionalny : Samolot
-	{ }
+	{
+		public Regionalny(double zasieg, string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc) { }
+	}
 	public class SamolotException : Exception { }
 	public class ZasiegException : SamolotException { }
 	public class LiczbaMiejscException : SamolotException { }
