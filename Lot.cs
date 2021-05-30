be0c105d3c27bf9f243c43fbe8d13078f523fe17
KornelG <kornelg2001@wp.pl>
2021-05-30 11:29:59 +0200

    Zmiany, zmiany jeszcze raz zmiany. Zmieniłem konstruktory kopiujące i poprawiłem pare błedów. Wydaje mi się, że wszystko powinno działać w porządku.

--- Lot.cs
+++ Lot.cs
@@ -5,31 +5,68 @@ namespace LiniaLotnicza
 
 	public class Lot
 	{
-		private ArrayList Rezerwacje;
+		private List<Rezerwacja> Rezerwacje;
 		private Samolot Samolot;
 		private Trasa Trasa;
 		private DateTime DataLotu;
+		public Lot() { }
+
+		//Zwykly konstruktor
 		public Lot(Samolot s, Trasa t, DateTime data)
-		{ this.DataLotu = data; this.Trasa = new Trasa(t); this.Samolot = new Samolot(s); Rezerwacje = new ArrayList()};
-		public ArrayList getRezerwacje() { return this.Rezerwacje; }
-		public getSamolot() { return this.Samolot; }
-		public getTrasa() { return this.Trasa; }
+		{ this.DataLotu = data; this.Trasa = new Trasa(t); this.Samolot = new Samolot(s); Rezerwacje = new List<Rezerwacja>(); }
+
+		// Konstruktor kopiujacy - tworzy nowa liste rezerwacji i przekopiowuje z niej rezerwacje do listy rezerwacji lotu.
+		public Lot(Lot lot)
+		{
+			this.Samolot = new Samolot(lot.getSamolot());
+			this.Trasa = new Trasa(lot.getTrasa());
+			List<Rezerwacja> rez = lot.getRezerwacje();
+			this.Rezerwacje = new List<Rezerwacja>(lot.getRezerwacje());
+		}
+		public List<Rezerwacja> getRezerwacje() { return this.Rezerwacje; }
+		public Samolot getSamolot() { return this.Samolot; }
+		public Trasa getTrasa() { return this.Trasa; }
 		public void dodajRezerwacje(Rezerwacja r) { Rezerwacje.Add(r); }
 		public void usunRezerwacje(Rezerwacja r)
 		{
-			//Metoda przechodzi przez cala liste rezerwacji i porownuje rezerwacje.
-			foreach (Rezerwacja re in Rezerwacje)
+			//Metoda przeglada cala liste rezerwacji i porownuje pola obiektow poprzez metode porownajRezerwacje, a nastepnie usuwa poszczegolne rezerwacje.
+			for (int i = 0; i < this.Rezerwacje.Count; i++)
 			{
-				if (porownajRezerwacje(r, re))
-					Rezerwacje.Remove(re);
+				if (r.porownajRezerwacje(this.Rezerwacje[i]))
+					Rezerwacje.RemoveAt(i);
 			}
 		}
+		// Metoda porownajLot porownuje poszczegolne pola i wywoluje prywatna metode porownajListyRezerwacji ktora porownuje listy rezerwacji i zwraca true jezeli listy sa takie same.
 
+		public bool porownajLot(Lot lot)
+		{
+			List<Rezerwacja> rez = lot.getRezerwacje();				 // Sprawdzenie czy listy
+			if (rez.Count != this.Rezerwacje.Count)			    	// posiadaja taka sama
+				return false;								       // liczbe elementow.
+
+			if (this.Samolot.porownajSamolot(new Samolot(lot.getSamolot()))&&this.Trasa.porownajTrase(new Trasa(lot.getTrasa()))&&this.DataLotu==lot.getDataLotu() && porownajRezerwacje(lot))
+				return true;
+			else
+				return false;
+		}
+		private bool porownajRezerwacje(Lot lot)
+		{
+			List<Rezerwacja> rez = lot.getRezerwacje();
+			bool zmienna = false;
+			for (int i = 0; i < rez.Count; i++)
+			{
+				if (rez[i].porownajRezerwacje(new Rezerwacja(this.Rezerwacje[i])))
+					zmienna = true;
+				else
+					return false;
+			}
+			return zmienna;
+		}
 		//Metoda porownanieRezerwacji wywoluje porownanie klientow
 		private bool porownajRezerwacje(Rezerwacja r, Rezerwacja re)
 		{
 			Klient k = r.getKlient(), kl = re.getKlient();
-			return (k.porownanie(kl));
+			return (k.porownajKlienta(kl));
 		}
 		public DateTime getDataLotu(){return this.DataLotu; }
 	}
