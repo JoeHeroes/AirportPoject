1c30f64746d1ada197bc8cea8beecd3f5b1685c6
KornelG <kornelg2001@wp.pl>
2021-06-03 11:43:08 +0200

    Zmiany konstruktorow samolotow, dodanie maksymalnego zasiegu w samolocie

--- Samoloty.cs
+++ Samoloty.cs
@@ -6,16 +6,18 @@ namespace LiniaLotnicza
 		protected double Zasieg;
 		protected string Id;
 		protected int LiczbaMiejsc;
+		protected double MaksymalnyZasieg;
 		public Samolot() { }
 		public Samolot(double zasieg, string id, int liczbamiejsc) {
-			if (zasieg <= 0)
-				throw new ZasiegException("Zasieg nie moze byc mniejszy od zera.");
+			if (zasieg <= 0 && zasieg > this.MaksymalnyZasieg)
+				throw new ZasiegException("Zasieg nie moze byc mniejszy od zera oraz wiekszy od maksymalnego zasiegu.");
 			if (liczbamiejsc <= 0)
 				throw new LiczbaMiejscException("Liczba miejsc nie moze byc mniejsza od zera.");
 			this.Zasieg = zasieg;
 			this.Id = id;
 			this.LiczbaMiejsc = liczbamiejsc; }
 		public double getZasieg() { return this.Zasieg; }
+		public double getMaksymalnyZasieg() { return this.MaksymalnyZasieg; }
 		public string getId(){return this.Id; }
 		public int getLiczbaMiejsc() { return this.LiczbaMiejsc; }
 		public override bool Equals(Object obj)
@@ -27,26 +29,29 @@ namespace LiniaLotnicza
 			else
 			{
 				Samolot s = (Samolot)obj;
-				return (this.Id == s.getId() && this.Zasieg == s.getZasieg() && this.LiczbaMiejsc == s.getLiczbaMiejsc());
+				return (this.Id == s.getId() && this.Zasieg == s.getZasieg()&& this.MaksymalnyZasieg== s.getMaksymalnyZasieg() && this.LiczbaMiejsc == s.getLiczbaMiejsc());
 			}
 		}
 	}
 	public class Dlugodystansowy : Samolot
 	{
-		public Dlugodystansowy(double zasieg, string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc)
+		public Dlugodystansowy(double zasieg,string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc)
 		{
+			this.MaksymalnyZasieg = 20000;
 		}
 	}
 	public class Sredniodystansowy : Samolot
 	{
 		public Sredniodystansowy(double zasieg, string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc)
 		{
+			this.MaksymalnyZasieg = 5000;
 		}
 	}
 	public class Regionalny : Samolot
 	{
 		public Regionalny(double zasieg, string id, int liczbamiejsc) : base(zasieg, id, liczbamiejsc)
 		{
+			this.MaksymalnyZasieg = 1000;
 		}
 	}
 	public class SamolotException : Exception 
